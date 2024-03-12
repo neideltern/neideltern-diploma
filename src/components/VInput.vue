6990ee518947a8c7093db9e58b2d3fe3630c5155
neideltern <neideltern@gmail.com>
2024-03-12 21:08:44 +0200

    input component (wip)

--- src/components/VInput.vue
+++ src/components/VInput.vue
@@ -5,12 +5,16 @@ defineProps({
     },
     icon: {
         type: String
+    },
+    disabled: {
+        type: Boolean,
+        default: false
     }
 })
 </script>
 
 <template>
-    <div class="input">
+    <div class="input" :inert="disabled">
         <input type="text" :placeholder />
         <i class="icon" v-if="icon">{{ icon }}</i>
     </div>
@@ -25,8 +29,31 @@ defineProps({
     padding: $gap-tn $gap-sm
     border-radius: $radius-md
     background: $element-foreground
+    box-shadow: inset 0 0 0 1px transparent
+    font-size: 14px
+    // animation
+    transition: background .2s ease-in-out, box-shadow .2s ease-in-out
+
+    &:focus-within
+        background: $element-foreground-darken
+        box-shadow: inset 0 0 0 1px $element-foreground
+
+    &[inert]
+        opacity: .6
 
     input
         height 100%
         flex-grow: 1
+        line-height: 1
+        font-weight: 420
+        font-family: $family-text
+        color: $text-secondary
+
+        &:placeholder
+            color: $text-dark
+
+    .icon
+        color: $text-tertiary
+
+    
 </style>
