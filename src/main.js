b57bb17568c01fb74ad347fb7ccbe05b7f7f1b67
neideltern <neideltern@gmail.com>
2024-03-18 17:31:36 +0200

    I18n Internationalization plugin setup

--- src/main.js
+++ src/main.js
@@ -1,12 +1,33 @@
+// vue app
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
-
 const app = createApp(App)
 
-app.use(createPinia())
+// router
+import router from './router'
 app.use(router)
 
+// pinia
+import { createPinia } from 'pinia'
+app.use(createPinia())
+
+// localization
+import { createI18n } from 'vue-i18n'
+
+import EN from '@/locale/en.json'
+import DE from '@/locale/de.json'
+import FR from '@/locale/fr.json'
+
+const i18n = createI18n({
+    locale: 'EN',
+    messages: {
+        EN: EN,
+        DE: DE,
+        FR: FR
+    }
+})
+
+app.use(i18n)
+
+// mount app
 app.mount('#app')
